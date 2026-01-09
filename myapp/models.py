eaa1d0b8223a43e0e5811aa7f3fa5ad4d937bc55
SWAFWANA SHERI <swafwanasheri17@gmail.com>
2026-01-09 12:38:08 +0530

    Initial commit – project setup

---
+++ myapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Users(models.Model):
+    name=models.CharField(max_length=100)
+    dob=models.DateField()
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    photo = models.CharField(max_length=100)
+    AUTH_USER=models.OneToOneField(User,on_delete=models.CASCADE)
+
+class Complaint(models.Model):
+    date=models.DateField()
+    complaint=models.CharField(max_length=500)
+    reply=models.CharField(max_length=500)
+    status=models.CharField(max_length=100)
+    USER=models.ForeignKey(Users,on_delete=models.CASCADE)
+
+class Review(models.Model):
+    date=models.DateField()
+    review=models.CharField(max_length=500)
+    rating=models.CharField(max_length=100)
+    USER = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class Log(models.Model):
+    date = models.DateField()
+    time=models.DateTimeField()
+    result=models.CharField(max_length=100)
+    USER = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
+
