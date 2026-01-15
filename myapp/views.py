5bb171215b3c16a4c8c8847e5508eff395f4a346
SWAFWANA <swafwanasheri17@gmail.com>
2026-01-15 12:10:45 +0530

    Function login change password complaint send reply done

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
+from myapp.models import Complaint, Users
 
 
 def login_get(request):
@@ -11,7 +14,22 @@ def loginindex_get(request):
     return render(request,'loginindex.html')
 
 def loginindex_post(request):
-    return render(request,'loginindex.html')
+    Username = request.POST["username"]
+    Password = request.POST["password"]
+    user=authenticate(request,username=Username,password=Password)
+    if user is not None:
+        login(request,user)
+        if user.groups.filter(name="admin"):
+            return redirect('/myapp/admin_home/')
+        else:
+            messages.error(request,"No such group")
+            return redirect('/myapp/loginindex_get/')
+    else:
+        messages.error(request, "No user found")
+        return redirect('/myapp/loginindex_get/')
+
+
+
 
 def forgot_get(request):
     return render(request,'forgotpassword.html')
@@ -25,25 +43,53 @@ def admin_home(request):
     return render(request,'admins/adminhomeindex.html')
 
 def viewcomplaint_get(request):
-    return render(request,'admins/viewcomplaint.html')
+    data=Complaint.objects.all()
+
+    return render(request,'admins/viewcomplaint.html',{'c':data})
 
 def viewlogs_get(request):
     return render(request,'admins/viewlogs.html')
 
 def viewusers_get(request):
-    return render(request,'admins/viewuser.html')
+    data=Users.objects.all()
+    return render(request,'admins/viewuser.html',{'Users': data})
 
 def changepassword_get(request):
     return render(request,'admins/changepassword.html')
 
 def changepassword_post(request):
-    return render(request,'admins/changepassword.html')
+    current_password=request.POST["currentpassword"]
+    new_password = request.POST["newpassword"]
+    confirm_password = request.POST["confirmpassword"]
+    user=request.user
+    if not user.check_password(current_password):
+        messages.error(request,"Invalid  Password")
+        return redirect('/myapp/changepassword_get/')
 
-def sentreply_get(request):
-    return render(request,'admins/sentreply.html')
+    if new_password != confirm_password:
+        messages.error(request, "Password doesn't match")
+        return redirect('/myapp/changepassword_get/')
+    user.set_password(new_password)
+    user.save()
+
+    return redirect('/myapp/loginindex_get/')
+
+
+def sentreply_get(request,id):
+    return render(request,'admins/sentreply.html',{'id':id})
 
 def sentreply_post(request):
-    return render(request,'admins/sentreply.html')
+    reply=request.POST["reply"]
+    id=request.POST["id"]
+
+    data=Complaint.objects.get(id=id)
+    data.reply=reply
+    data.status="Replied"
+    data.save()
+    return redirect("/myapp/viewcomplaint_get/")
+
+
+
 
 # U S E R
 
